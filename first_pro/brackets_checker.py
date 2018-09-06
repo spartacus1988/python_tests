ff9e472b122d4aad1522363b605ea448c86d3392
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2018-09-07 01:25:40 +0300

    almost fine

--- first_pro/brackets_checker.py
+++ first_pro/brackets_checker.py
@@ -1,15 +1,15 @@
 import re
-import sys
+import sys 
 import argparse
 
 
 def createParser():
 	parser = argparse.ArgumentParser()
-	#parser.add_argument ('expression', nargs='?', default='esdfd((esdf)(esdf')
-	parser.add_argument ('-e', '--expression', default='esdfd((esdf)(esdf')
+	parser.add_argument ('expression', nargs='?', default='esdfd((esdf)(esdf')
+	#parser.add_argument ('-e', '--expression', default='esdfd((esdf)(esdf')
 	return parser
 
-def brackets_checker(text):
+def brackets_checker_re(text):
 	pattern = re.compile(r'\([^\)]+$')
 	#result = re.findall(pattern, text)
 	#print(result)
@@ -19,6 +19,38 @@ def brackets_checker(text):
 			return result
 
 
+def brackets_checker_split(text):
+	text = text.split('(')
+	#print (text)
+	n = 0
+	count = len(text)
+	#print(count)
+
+	for i in range(count):
+		#print(i)
+		#print ("\n\r")
+
+		piece = text[-(i+1)] 
+		#print(piece)
+
+		if ')' not in piece:
+			n+=1
+		else:
+			break
+
+	#print(n)
+	pos = n-1 if n == count else n
+
+	for x in range(pos):
+		#print(text)
+		text.pop() 
+
+	res = '('.join(text)
+	print(res)
+	return res
+
+
+
 if __name__ == '__main__':
 
 	parser = createParser()
@@ -26,4 +58,5 @@ if __name__ == '__main__':
 	#print (namespace)
 	text = str(namespace.expression)
 
-	res = brackets_checker(text)
+	res_re = brackets_checker_re(text)
+	res_split = brackets_checker_split(text)
--- first_pro/test_brackets_checker.py
+++ first_pro/test_brackets_checker.py
@@ -1,15 +1,55 @@
 import unittest
+import sys
 import brackets_checker
-
-
+from optparse import OptionParser
 
 class TestMethods(unittest.TestCase):
 
-	def test_brackets_default_expression(self):
-			res = brackets_checker.brackets_checker('esdfd((esdf)(esdf')
+	# def __init__(self, testname, expression):
+	# 	super(TestMethods, self).__init__(testname)
+	# 	self.expression = expression
+
+	def test_brackets_re_default_expression(self):
+			res = brackets_checker.brackets_checker_re('esdfd((esdf)(esdf')
 			self.assertEqual(res, 'esdfd((esdf)')
 
+	def test_brackets_split_default_expression(self):
+			res = brackets_checker.brackets_checker_split('esdfd((esdf)(esdf')
+			self.assertEqual(res, 'esdfd((esdf)')
+
+	def test_brackets_compare_two_methods(self):
+			res1 = brackets_checker.brackets_checker_re(arg_for_test)
+			res2 = brackets_checker.brackets_checker_split(arg_for_test)
+			#print(res1)
+			#print(res2)
+			self.assertEqual(res1, res2)
+
 if __name__ == '__main__':
+	parser = OptionParser()
+	parser.add_option("-e", "--expression",
+					  action="store_true", dest="expression", default=False,
+					  help="add arg for third test")    
+	(options, args) = parser.parse_args()
+
+	if options.expression:
+		#print(options.expression)
+		#print(options)
+		#print(args)
+		arg_for_test = args[0]
+		#print(arg_for_test)
+
+	# remove our args because we don't want to send them to unittest
+	for x in sum([h._long_opts+h._short_opts for h in parser.option_list],[]):
+		#print(x)
+		#print(sys.argv)
+		if x in sys.argv:
+			#print("TRUE TRUESHNOE")
+			sys.argv.remove(x)
+			sys.argv.remove(arg_for_test)
+			#print(sys.argv)
+
+	#print('now')
+	#print(sys.argv)
 
 	unittest.main()
 	
