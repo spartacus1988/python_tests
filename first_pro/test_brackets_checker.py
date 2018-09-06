b3b5c196fbbda09eb8ebe4a6064ebea893c5b43d
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2018-09-07 01:32:41 +0300

    fine

--- first_pro/test_brackets_checker.py
+++ first_pro/test_brackets_checker.py
@@ -5,10 +5,6 @@ from optparse import OptionParser
 
 class TestMethods(unittest.TestCase):
 
-	# def __init__(self, testname, expression):
-	# 	super(TestMethods, self).__init__(testname)
-	# 	self.expression = expression
-
 	def test_brackets_re_default_expression(self):
 			res = brackets_checker.brackets_checker_re('esdfd((esdf)(esdf')
 			self.assertEqual(res, 'esdfd((esdf)')
@@ -20,8 +16,6 @@ class TestMethods(unittest.TestCase):
 	def test_brackets_compare_two_methods(self):
 			res1 = brackets_checker.brackets_checker_re(arg_for_test)
 			res2 = brackets_checker.brackets_checker_split(arg_for_test)
-			#print(res1)
-			#print(res2)
 			self.assertEqual(res1, res2)
 
 if __name__ == '__main__':
@@ -32,24 +26,13 @@ if __name__ == '__main__':
 	(options, args) = parser.parse_args()
 
 	if options.expression:
-		#print(options.expression)
-		#print(options)
-		#print(args)
 		arg_for_test = args[0]
-		#print(arg_for_test)
 
 	# remove our args because we don't want to send them to unittest
 	for x in sum([h._long_opts+h._short_opts for h in parser.option_list],[]):
-		#print(x)
-		#print(sys.argv)
 		if x in sys.argv:
-			#print("TRUE TRUESHNOE")
 			sys.argv.remove(x)
 			sys.argv.remove(arg_for_test)
-			#print(sys.argv)
-
-	#print('now')
-	#print(sys.argv)
 
 	unittest.main()
 	
